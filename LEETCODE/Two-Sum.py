f89f7815986f158444e18ce55c6f506b2c386c5a
shynhsiri <shynhsiri@gmail.com>
2024-08-30 21:30:16 +0330

    Two Sum detail added

--- LEETCODE/Two-Sum.py
+++ LEETCODE/Two-Sum.py
@@ -8,3 +8,18 @@ class Solution:
                 return [prevMap[diff], i]
             prevMap[n] = i
         return
+
+"""
+63 / 63 test cases passed.
+Status: Accepted
+Runtime: 59 ms
+Memory Usage: 17.7 MB
+
+
+You are here!
+Your runtime beats 64.77 % of python3 submissions.
+
+
+You are here!
+Your memory usage beats 56.51 % of python3 submissions.
+"""
