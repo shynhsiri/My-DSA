4a7653196444a5e8b3c2bfc3478e56d2fcf1f54e
shynhsiri <shynhsiri@gmail.com>
2024-08-31 21:37:03 +0330

    leetcode 121

---
+++ LEETCODE/121.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        max_profit = 0
+
+        while r < len(prices):
+            if prices[r] > prices[l]:
+                profit = prices[r] - prices[l]
+                max_profit = max(max_profit, profit)
+            else:
+                l = r
+            r += 1
+        return max_profit
+
+
+"""
+Accepted
+
+Runtime
+767ms
+Beats
+32.68%
+
+Memory
+27.45MB
+Beats
+37.33%
+"""
