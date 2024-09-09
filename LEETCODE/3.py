350503c2fac1a8d15eea23ec4111ee6e26519dcf
shynhsiri <shynhsiri@gmail.com>
2024-09-09 07:52:54 +0330

    leetcode 3

---
+++ LEETCODE/3.py
@@ -0,0 +1,32 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        charSet = set()
+        l = 0
+        res = 0
+
+        for r in range(len(s)):
+            while s[r] in charSet:
+                charSet.remove(s[l])
+                l += 1
+            charSet.add(s[r])
+            res = max(res, r - l + 1)
+        return res
+
+
+"""
+Accepted
+
+Runtime
+55
+ms
+Beats
+59.36%
+
+Memory
+16.44
+MB
+Beats
+98.75%
+
+9/9/2024
+"""
